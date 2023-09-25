1b0c6e0ef9d46c729b030f0dfc0ecf92362b5dda
Alyona1619 <108504553+Alyona1619@users.noreply.github.com>
2023-09-26 00:00:26 +0800

    Update average.asm

--- average.asm
+++ average.asm
@@ -1,5 +1,30 @@
+%macro pushd 0
+	push rax
+	push rbx
+	push rcx
+	push rdx
+%endmacro
+
+%macro popd 0
+	pop rdx
+	pop rcx
+	pop rbx
+	pop rax
+%endmacro
+
+%macro print 1
+    pushd
+    push rdi
+    mov rdi, message
+    mov rsi, %1
+    call printf
+    pop rdi
+    popd
+%endmacro
+
 section   .text
-global    _start
+global    main
+extern printf
 
 _start:
 
